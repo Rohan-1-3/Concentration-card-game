8e7fb0703121ce4ae730531c474794014c93d433
Rohan-1-3 <rohanshrestha130@gmail.com>
2024-06-15 12:51:34 +0545

    added favicon and github link

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ function App() {
         <FancyButton handleClick={() => setStartGame(true)} buttonName="Start Game" />
       )}
         <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline text-blue-500">Rohan-1-3</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/Rohan-1-3/Concentration-card-game" className="hover:underline text-blue-500">Rohan-1-3</a>. All Rights Reserved.
             </span>
           </footer>
     </div>
