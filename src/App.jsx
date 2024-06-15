66f17976e0eca78dac5d35f29a13c741b8b89bc7
Rohan-1-3 <rohanshrestha130@gmail.com>
2024-06-15 12:36:14 +0545

    added proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import { useFetchDogs } from './customHooks/useFetchDogs';
 import DogCard from './DogCard';
 import Timer from './Timer';
-
+import PropTypes from "prop-types"
 
 function FancyButton({handleClick, buttonName}) {
   return (<button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
@@ -82,6 +82,11 @@ function App() {
     </div>
 
   );
+}
+
+FancyButton.propTypes = {
+    handleClick : PropTypes.func,
+    buttonName : PropTypes.string
 }
 
 export default App;
